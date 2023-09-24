712423f29676c2ad9bb981afe5703f849a15e7ba
Nyanibah Escoffery <naomi@diamondrecovery.net>
2023-09-24 18:18:21 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+      const rightNumbers = dodger.style.left.replace("px", "");
+      const right = parseInt(rightNumbers, 10);
+  
+      dodger.style.left = `${right + 1}px`;
+    }
+  });
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+    if (right < 360) {
+        dodger.style.left = `${right + 1}px`;
+      }
+
+}
